d19587f5456a1a611b42f189623243f5cd8354e8
JTCJTC <97971137+TheCockslapper@users.noreply.github.com>
2022-06-29 19:14:32 +0200

    Add files via upload

---
+++ Projects Chapter 2/whitespace_strings.py
@@ -0,0 +1,18 @@
+# If you do not know how to make "\", press alt and type 92 in your keypad if all else fails.
+
+# print("Python")
+# print("\tPython")
+
+# print("Languages:\nPython\nC\nJavaScript")
+# print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# favorite_language = 'python '
+# favorite_language.rstrip()
+
+# favorite_language = 'python '
+# favorite_language = favorite_language.rstrip()
+
+favorite_language = ' python '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
