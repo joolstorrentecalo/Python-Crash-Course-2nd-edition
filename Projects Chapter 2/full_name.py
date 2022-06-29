2cae519f404907bb65bcac19f646faa219d1b4f2
JTCJTC <97971137+TheCockslapper@users.noreply.github.com>
2022-06-29 18:35:06 +0200

    Add files via upload

---
+++ Projects Chapter 2/full_name.py
@@ -0,0 +1,6 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name}  {last_name}"
+# print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
---
+++ Projects Chapter 2/name.py
@@ -0,0 +1,6 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
